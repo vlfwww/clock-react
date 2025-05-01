0487b71bb2302b77be2bd2c95c2854a43b1ed459
vlfwww <vlfwww@outlook.com>
2025-05-01 22:41:14 +0300

    added clock, useEffect used

--- app/src/App.js
+++ app/src/App.js
@@ -7,9 +7,39 @@ function App() {
   const [time,savetime] = useState('');
   useEffect(()=>{
     const date = new Date();
-    saveDate(`${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`)
+    const mounth = date.getMonth()+1;
+    let day = date.getDate();
+    let hours = date.getHours();
+    let mins = date.getMinutes();
+    let sec = date.getSeconds();
+    if(day<10){
+      day = `0${day}`
+    }
+    if(hours<10){
+      hours = `0${hours}`
+    }
+    if(mins<10){
+      mins = `0${mins}`
+    }
+    if(sec<10){
+      sec = `0${sec}`
+    }
+    switch(mounth){
+      case 1: saveDate(`${day} Января, ${date.getFullYear()}`); break;
+      case 2: saveDate(`${day} Февраля, ${date.getFullYear()}`); break;
+      case 3: saveDate(`${day} Марта, ${date.getFullYear()}`); break;
+      case 4: saveDate(`${day} Апреля, ${date.getFullYear()}`); break;
+      case 5: saveDate(`${day} Мая, ${date.getFullYear()}`); break;
+      case 6: saveDate(`${day} Июня, ${date.getFullYear()}`); break;
+      case 7: saveDate(`${day} Июля, ${date.getFullYear()}`); break;
+      case 8: saveDate(`${day} Августа, ${date.getFullYear()}`); break;
+      case 9: saveDate(`${day} Сентября, ${date.getFullYear()}`); break;
+      case 10: saveDate(`${day} Октября, ${date.getFullYear()}`); break;
+      case 11: saveDate(`${day} Ноября, ${date.getFullYear()}`); break;
+      case 12: saveDate(`${day} Декабря, ${date.getFullYear()}`); break;
+    }
     const n = setInterval(()=>{
-      savetime(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+      savetime(`${hours}:${mins}:${sec}`)
     },1000)
     return ()=>{
       clearInterval(n);
